78dbd8beb00c5b2bb5d461aa651b3c574a8bba59
Vu Nguyen <vunguyen@Vus-MacBook-Pro.local>
2015-03-18 22:10:40 -0400

    new table

--- src/sqls/ContactLenses.sql
+++ src/sqls/ContactLenses.sql
@@ -1,35 +1,40 @@
-DROP TABLE IF EXISTS ContactLenses;
-CREATE TABLE 
-	ContactLenses (ID INT PRIMARY KEY,
-		AGE INT,
-		SPECPRESC INT,
-		ASTIGMATIC INT,
-		TEARPR INT,
-		TARGET VARCHAR(15)
-	);
+DROP TABLE IF EXISTS CLENSES;
+CREATE TABLE
+CLENSES (ID INT PRIMARY KEY,
+AGE VARCHAR(255),
+SPR VARCHAR(255),
+AST VARCHAR(255),
+TPR VARCHAR(255),
+LNS VARCHAR(255)
+);
 
-INSERT INTO ContactLenses VALUES(1, 1, 1, 1, 1, 'No lenses');
-INSERT INTO ContactLenses VALUES(2, 1, 1, 1, 2, 'Soft lenses');
-INSERT INTO ContactLenses VALUES(3, 1, 1, 2, 1, 'No lenses');
-INSERT INTO ContactLenses VALUES(4, 1, 1, 2, 2, 'Hard lenses');
-INSERT INTO ContactLenses VALUES(5, 1, 2, 1, 1, 'No lenses');
-INSERT INTO ContactLenses VALUES(6, 1, 2, 1, 2, 'Soft lenses');
-INSERT INTO ContactLenses VALUES(7, 1, 2, 2, 1, 'No lenses');
-INSERT INTO ContactLenses VALUES(8, 1, 2, 2, 2, 'Hard lenses');
-INSERT INTO ContactLenses VALUES(9, 2, 1, 1, 1, 'No lenses');
-INSERT INTO ContactLenses VALUES(10, 2, 1, 1, 2, 'Soft lenses');
-INSERT INTO ContactLenses VALUES(11, 2, 1, 2, 1, 'No lenses');
-INSERT INTO ContactLenses VALUES(12, 2, 1, 2, 2, 'Hard lenses');
-INSERT INTO ContactLenses VALUES(13, 2, 2, 1, 1, 'No lenses');
-INSERT INTO ContactLenses VALUES(14, 2, 2, 1, 2, 'Soft lenses');
-INSERT INTO ContactLenses VALUES(15, 2, 2, 2, 1, 'No lenses');
-INSERT INTO ContactLenses VALUES(16, 2, 2, 2, 2, 'No lenses');
-INSERT INTO ContactLenses VALUES(17, 3, 1, 1, 1, 'No lenses');
-INSERT INTO ContactLenses VALUES(18, 3, 1, 1, 2, 'No lenses');
-INSERT INTO ContactLenses VALUES(19, 3, 1, 2, 1, 'No lenses');
-INSERT INTO ContactLenses VALUES(20, 3, 1, 2, 2, 'Hard lenses');
-INSERT INTO ContactLenses VALUES(21, 3, 2, 1, 1, 'No lenses');
-INSERT INTO ContactLenses VALUES(22, 3, 2, 1, 2, 'Soft lenses');
-INSERT INTO ContactLenses VALUES(23, 3, 2, 2, 1, 'No lenses');
-INSERT INTO ContactLenses VALUES(24, 3, 2, 2, 2, 'No lenses');
+INSERT INTO CLENSES VALUES(1, 'young', 'myope', 'no', 'reduced', 'No contact lenses');
+INSERT INTO CLENSES VALUES(2, 'young', 'myope', 'no', 'normal', 'Soft contact lenses');
+INSERT INTO CLENSES VALUES(3, 'young', 'myope', 'yes', 'reduced', 'No contact lenses');
+INSERT INTO CLENSES VALUES(4, 'young', 'myope', 'yes', 'normal', 'Hard contact lenses');
+INSERT INTO CLENSES VALUES(5, 'young', 'hypermetrope', 'no', 'reduced', 'No contact lenses');
+INSERT INTO CLENSES VALUES(6, 'young', 'hypermetrope', 'no', 'normal', 'Soft contact lenses');
+INSERT INTO CLENSES VALUES(7, 'young', 'hypermetrope', 'yes', 'reduced', 'No contact lenses');
+INSERT INTO CLENSES VALUES(8, 'young', 'hypermetrope', 'yes', 'normal', 'Hard contact lenses');
+INSERT INTO CLENSES VALUES(9, 'pre-presbyopic', 'myope', 'no', 'reduced', 'No contact lenses');
+INSERT INTO CLENSES VALUES(10, 'pre-presbyopic', 'myope', 'no', 'normal', 'Soft contact lenses');
+INSERT INTO CLENSES VALUES(11, 'pre-presbyopic', 'myope', 'yes', 'reduced', 'No contact lenses');
+INSERT INTO CLENSES VALUES(12, 'pre-presbyopic', 'myope', 'yes', 'normal', 'Hard contact lenses');
+INSERT INTO CLENSES VALUES(13, 'pre-presbyopic', 'hypermetrope', 'no', 'reduced', 'No contact lenses');
+INSERT INTO CLENSES VALUES(14, 'pre-presbyopic', 'hypermetrope', 'no', 'normal', 'Soft contact lenses');
+INSERT INTO CLENSES VALUES(15, 'pre-presbyopic', 'hypermetrope', 'yes', 'reduced', 'No contact lenses');
+INSERT INTO CLENSES VALUES(16, 'pre-presbyopic', 'hypermetrope', 'yes', 'normal', 'No contact lenses');
+INSERT INTO CLENSES VALUES(17, 'presbyopic', 'myope', 'no', 'reduced', 'No contact lenses');
+INSERT INTO CLENSES VALUES(18, 'presbyopic', 'myope', 'no', 'normal', 'No contact lenses');
+INSERT INTO CLENSES VALUES(19, 'presbyopic', 'myope', 'yes', 'reduced', 'No contact lenses');
+INSERT INTO CLENSES VALUES(20, 'presbyopic', 'myope', 'yes', 'normal', 'Hard contact lenses');
+INSERT INTO CLENSES VALUES(21, 'presbyopic', 'hypermetrope', 'no', 'reduced', 'No contact lenses');
+INSERT INTO CLENSES VALUES(22, 'presbyopic', 'hypermetrope', 'no', 'normal', 'Soft contact lenses');
+INSERT INTO CLENSES VALUES(23, 'presbyopic', 'hypermetrope', 'yes', 'reduced', 'No contact lenses');
+INSERT INTO CLENSES VALUES(24, 'presbyopic', 'hypermetrope', 'yes', 'normal', 'No contact lenses');
 
+
+CALL DECISIONTREE('CLENSES', 'AGE', 'SPR', 'AST', 'TPR', 'LNS');
+
+ALTER TABLE CLENSES
+ADD COLUMN PREDICTION VARCHAR(255) AS CLASSIFY('CONTACTLENSES', AGE, SPR, AST, TPR, LNS) AFTER LNS
