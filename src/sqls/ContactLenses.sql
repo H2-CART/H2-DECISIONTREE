1efd59dace29cb92dcbc587e7e97c15bc664f535
xxu52 <silviaxxy@gmail.com>
2015-04-22 21:55:24 -0500

    add sql sampls

---
+++ src/sqls/ContactLenses.sql
@@ -0,0 +1,40 @@
+DROP TABLE IF EXISTS CLENSES;
+CREATE TABLE
+CLENSES (ID INT PRIMARY KEY,
+AGE VARCHAR(255),
+SPR VARCHAR(255),
+AST VARCHAR(255),
+TPR VARCHAR(255),
+LNS VARCHAR(255)
+);
+
+INSERT INTO CLENSES VALUES(1, 'young', 'myope', 'no', 'reduced', 'No contact lenses');
+INSERT INTO CLENSES VALUES(2, 'young', 'myope', 'no', 'normal', 'Soft contact lenses');
+INSERT INTO CLENSES VALUES(3, 'young', 'myope', 'yes', 'reduced', 'No contact lenses');
+INSERT INTO CLENSES VALUES(4, 'young', 'myope', 'yes', 'normal', 'Hard contact lenses');
+INSERT INTO CLENSES VALUES(5, 'young', 'hypermetrope', 'no', 'reduced', 'No contact lenses');
+INSERT INTO CLENSES VALUES(6, 'young', 'hypermetrope', 'no', 'normal', 'Soft contact lenses');
+INSERT INTO CLENSES VALUES(7, 'young', 'hypermetrope', 'yes', 'reduced', 'No contact lenses');
+INSERT INTO CLENSES VALUES(8, 'young', 'hypermetrope', 'yes', 'normal', 'Hard contact lenses');
+INSERT INTO CLENSES VALUES(9, 'pre-presbyopic', 'myope', 'no', 'reduced', 'No contact lenses');
+INSERT INTO CLENSES VALUES(10, 'pre-presbyopic', 'myope', 'no', 'normal', 'Soft contact lenses');
+INSERT INTO CLENSES VALUES(11, 'pre-presbyopic', 'myope', 'yes', 'reduced', 'No contact lenses');
+INSERT INTO CLENSES VALUES(12, 'pre-presbyopic', 'myope', 'yes', 'normal', 'Hard contact lenses');
+INSERT INTO CLENSES VALUES(13, 'pre-presbyopic', 'hypermetrope', 'no', 'reduced', 'No contact lenses');
+INSERT INTO CLENSES VALUES(14, 'pre-presbyopic', 'hypermetrope', 'no', 'normal', 'Soft contact lenses');
+INSERT INTO CLENSES VALUES(15, 'pre-presbyopic', 'hypermetrope', 'yes', 'reduced', 'No contact lenses');
+INSERT INTO CLENSES VALUES(16, 'pre-presbyopic', 'hypermetrope', 'yes', 'normal', 'No contact lenses');
+INSERT INTO CLENSES VALUES(17, 'presbyopic', 'myope', 'no', 'reduced', 'No contact lenses');
+INSERT INTO CLENSES VALUES(18, 'presbyopic', 'myope', 'no', 'normal', 'No contact lenses');
+INSERT INTO CLENSES VALUES(19, 'presbyopic', 'myope', 'yes', 'reduced', 'No contact lenses');
+INSERT INTO CLENSES VALUES(20, 'presbyopic', 'myope', 'yes', 'normal', 'Hard contact lenses');
+INSERT INTO CLENSES VALUES(21, 'presbyopic', 'hypermetrope', 'no', 'reduced', 'No contact lenses');
+INSERT INTO CLENSES VALUES(22, 'presbyopic', 'hypermetrope', 'no', 'normal', 'Soft contact lenses');
+INSERT INTO CLENSES VALUES(23, 'presbyopic', 'hypermetrope', 'yes', 'reduced', 'No contact lenses');
+INSERT INTO CLENSES VALUES(24, 'presbyopic', 'hypermetrope', 'yes', 'normal', 'No contact lenses');
+
+
+CALL DECISIONTREE('CLENSES', 'AGE', 'SPR', 'AST', 'TPR', 'LNS');
+
+ALTER TABLE CLENSES
+ADD COLUMN PREDICTION VARCHAR(255) AS CLASSIFY('CONTACTLENSES', AGE, SPR, AST, TPR, LNS) AFTER LNS
---
+++ src/sqls/MarineAnimal.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS MarineAnimal;
+CREATE TABLE 
+	MarineAnimal(ID INT PRIMARY KEY, 
+		NOSURFACE INT,
+		FLAPPERS INT,
+		FISH VARCHAR(3)
+	);
+
+INSERT INTO MarineAnimal VALUES(1, 1, 1, 'Yes');
+INSERT INTO MarineAnimal VALUES(2, 1, 1, 'Yes');
+INSERT INTO MarineAnimal VALUES(3, 1, 0, 'No');
+INSERT INTO MarineAnimal VALUES(4, 0, 1, 'No');
+INSERT INTO MarineAnimal VALUES(5, 0, 1, 'No');
